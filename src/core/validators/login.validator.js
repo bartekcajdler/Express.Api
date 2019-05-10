3338d98b287c4084c870333934165dc6ee7f1544
Bartłomiej Cajdler <bartekcajdler@webfabric.pl>
2019-05-10 10:34:12 +0200

    Removed *.js files

--- src/core/validators/login.validator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var joi = require("joi");
-exports.loginValidator = joi.object().keys({
-    login: joi.string().required(),
-    password: joi.string().required(),
-});
